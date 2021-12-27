621952aa81286b79b05de5904a3381ee3ae8ad64
Maureen McCarty <stardustgirl@gmail.com>
2021-12-27 16:24:23 -0500

    creating repository and adding files

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 /*
-User can select from a drop-down menu a top, bottom, socks, shoes, headwear, and bag to make a complete outfit from the Animal Crossing New Horizons inventory
+This is a test document
 */
 
 //fetch(url, { headers: { 'X-API-KEY': apiKey } })
@@ -7,21 +7,70 @@ User can select from a drop-down menu a top, bottom, socks, shoes, headwear, and
 // API KEY: 0d81853e-53b2-4ffa-a430-c049b6d1c02f
 //end of notes at top, let the fun begin
 
-//OMG THIS WORKS!!! 
-let baseURL = "https://api.nookipedia.com/nh/clothing?category=headware";
+//for a single category:
+//let baseURL = "https://api.nookipedia.com/nh/clothing?category=headwear";
 
+const baseURL = "https://api.nookipedia.com/villagers?species=Dog";
 
+let formElement = document.getElementById("search-form");
+let listElement = document.getElementById("dog-name");
 
-//define what each button brings back
-let buttonElement = document.getElementById("clothing-tops");
-
-
-
-
-
+function fetchDogs() {
 
 fetch(baseURL, { headers: { "X-API-KEY": "0d81853e-53b2-4ffa-a430-c049b6d1c02f" } })
 
-.then(response => response.json())
- .then(jsonData => console.log(jsonData));
+.then(response => {
+    return response.json();
+})
+.then(jsonData => {
+    console.log(jsonData);
+
+    for(let dog of jsonData) {
+        displayDog(dog);
+    }
+    
+})
+
+}
+
+fetchDogs();
+
+
+// stuff to go in boxes comes from (dogData)
+function displayDog(dogData) {
+
+    //boxes to pack all the items in
+    let dogHouse = document.createElement("ul");
+    let dogName = document.createElement("h2");
+    let dogImage = document.createElement("img");
+    let dogQuote = document.createElement("p");
+    
+
+    //stuff that goes in the boxes above
+    dogName.innerText = dogData.name;
+    dogImage.src = dogData.image_url;
+    dogQuote.innerText = dogData.quote;
+    listElement = dogData.name;
+
+    //labels for the boxes
+    dogHouse.classList.add("dog-house");
+    dogName.classList.add("dog-name");
+    dogImage.classList.add("dog-image");
+    dogQuote.classList.add("dog-quote");
+
+    //appendChild seals the boxes and puts them in the bigger box, Doghouse is the big box, the stuff after appendChild are the smaller boxes
+        dogHouse.appendChild(dogName);
+        dogHouse.appendChild(dogImage);
+        dogHouse.appendChild (dogQuote);
+
+    formElement.appendChild(dogHouse); 
+
+}
+
+
+
+
+
+
+
 
